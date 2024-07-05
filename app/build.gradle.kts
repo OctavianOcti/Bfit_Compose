965a2b83f3df2e7ee06c58293f757f2073f96c07
Octavian Octi <octavianmarian295@yahoo.com>
2024-07-06 00:31:58 +0300

    Login and registration screen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    // ViewModel utilities for Compose
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
